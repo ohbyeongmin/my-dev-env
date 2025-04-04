ee73885a14bae975f918679813b970e641a97c73
0x2biimi <0x2biimi@gmail.com>
2025-04-04 15:40:20 +0900

    lspconfig

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,18 +4,9 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls" }
+local servers = { "html", "cssls", "gopls", "rust_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 local util = require "lspconfig/util"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
 
 lspconfig.gopls.setup {
   cmd = {"gopls"},
@@ -31,6 +22,16 @@ lspconfig.gopls.setup {
     }
   },
 }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
